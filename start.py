b36200f8bfc0bf99dcd3c3fea2776742ea194392
V1INTIATIVE <shreyanshlakra724@gmail.com>
2026-07-19 15:25:52 +0530

    final

--- start.py
+++ start.py
@@ -1,11 +1,14 @@
-from re import search
-
+from datetime import datetime
+import requests 
 from skyfield.api import load 
 import math 
+import time
+import os
 
 
 
 url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle"
+
 satellites = load.tle_file(url)
 
 
@@ -14,38 +17,52 @@ sat_name = input("Enter the satellite name/ Norad ID you want to track:").strip(
 found = None 
 
 for a in satellites:
-    if sat_name in a.name.upper() or sat_name == str(a.norad_id):
+    if sat_name in a.name.upper():
         found = a
         break
     
-    if search.isdigit():
-        if int(search) == a.norad_id:
-            found = a
-            break
+    if sat_name.isdigit() and int(sat_name) == a.model.satnum:
+        found = a
+        break
         
+
+
 
 if found is None:
     print("Satellite not found.")
     exit()
 
 ts=load.timescale()
-t=ts.now()
+
+try:
+    while True:
+        t = ts.now()
 
 
-subpoint = found.at(t).subpoint()
+        subpoint = found.at(t).subpoint()
 
-velocity = found.at(t).velocity.km_per_s
+        velocity = found.at(t).velocity.km_per_s
 
-actual_velocity = math.sqrt(
-    velocity[0]**2 +
-    velocity[1]**2 +
-    velocity[2]**2
-) #the velocity that we are retrieving from the satellite is a vector quantity hence, we find it's magnitude.
+        actual_velocity_ms = math.sqrt(
+        velocity[0]**2 +
+        velocity[1]**2 +
+        velocity[2]**2
+)       #the velocity that we are retrieving from the satellite is a vector quantity hence, we find it's magnitude.
 
-print(f'Tracking satellite: {found.name} (NORAD ID: {found.norad_id})')
-print(f"Satellite: {found.name}")
-print(f"Latitude: {subpoint.latitude.degrees:.6f}°")
-print(f"Longitude: {subpoint.longitude.degrees:.6f}°")
-print(f"Altitude: {subpoint.elevation.m:.2f} meters")
-print(f"Distance from Earth's surface: {found.at(t).distance().km - 6371:.2f} km")
-print(f"Actual Velocity: {actual_velocity:.2f} km/s")
+        actual_velocity_kmh = actual_velocity_ms * 3.6 
+
+        print(f'Tracking satellite: {found.name} (NORAD ID: {found.model.satnum})')
+        print(f"Latitude: {subpoint.latitude.degrees:.6f}°")
+        print(f"Longitude: {subpoint.longitude.degrees:.6f}°")
+        print(f"Altitude: {subpoint.elevation.km:.2f} km")
+        print(f"Actual Velocity: {actual_velocity_kmh:.2f} km/h")
+        print(f"Timestamp: {datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')} UTC")
+
+
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("\nTracking stopped.")
+    exit()
+
+
+
